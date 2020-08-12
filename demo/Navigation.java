79231c6830a8748e377d56cd60a67b5af45ca6e4
Amnsingh <amnsingh36@gmail.com>
2020-08-12 20:39:16 +0530

    Added Navigation.java

---
+++ demo/Navigation.java
@@ -0,0 +1,26 @@
+package demo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Navigation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://www.amazon.com/");
+		driver.navigate().to("https://www.flipkart.com/");
+		//back and forward button simulation
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+	}
+
+}
